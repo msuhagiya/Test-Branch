3311ff2b35d2447654be08e1148d285d8facf41d
msuhagiya <suhagiya793@gmail.com>
2021-10-30 17:33:29 +0100

    I added some branch

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,6 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("I have created new branch");
+        System.out.println("I tested new branch");
     }
 }
